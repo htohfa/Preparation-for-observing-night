45de918a33c44aff930c97494f5b3c1932bcc28e
Hurum Maksora Tohfa <ttohfa@brynmawr.edu>
2023-05-06 12:21:05 -0700

    Update observing.py

--- observing.py
+++ observing.py
@@ -2,11 +2,14 @@ import argparse
 import time
 import numpy as np
 from bs4 import BeautifulSoup
+from astropy.coordinates import Angle
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from astropy.coordinates import SkyCoord
 from astropy.time import Time
 from astropy import units as u
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 import ephem
 from astroplan import Observer, FixedTarget
 
@@ -28,6 +31,39 @@ def get_ra_dec(driver, query):
         return ra, dec
     else:
         return None, None
+
+
+
+def save_airmass_chart(target_list, date, screenshot_file="airmass_chart.png"):
+    driver = webdriver.Chrome()
+
+    driver.get("http://catserver.ing.iac.es/staralt/index.php")
+
+    year, month, day = date.strftime("%Y-%B-%d").split("-")
+    Select(driver.find_element(By.NAME, "form[day]")).select_by_visible_text(day)
+    Select(driver.find_element(By.NAME, "form[month]")).select_by_visible_text(month)
+    Select(driver.find_element(By.NAME, "form[year]")).select_by_visible_text(year)
+
+    Select(driver.find_element(By.NAME, "form[obs_name]")).select_by_value("Mauna Kea Observatory (Hawaii, USA)")
+
+    coord_string = "\n".join([
+        f"{name.replace(' ', '')} {ra.replace('h', ' ').replace('m', ' ').replace('s', ' ')} {dec.replace('d', ' ').replace('m', ' ').replace('s', ' ')}"
+        for name, (ra, dec) in target_list
+    ])
+    driver.find_element(By.NAME, "form[coordlist]").send_keys(coord_string)
+
+    driver.find_element(By.NAME, "submit").click()
+
+    time.sleep(3)
+    driver.switch_to.window(driver.window_handles[-1])
+
+    driver.save_screenshot(screenshot_file)
+
+    driver.quit()
+
+
+
+
 
 def main(targets):
     observing_location = Observer(longitude=-155.0903 * u.deg,
@@ -77,11 +113,20 @@ def main(targets):
     best_day = start_time + best_day_idx * u.day
 
     print("RA and Dec values of targets:")
+    print("Name         RA (hh mm ss)    Dec (±dd mm ss)")
     for target, (ra, dec) in zip(targets, ra_dec_list):
-        print(f"{target}: RA = {ra}, Dec = {dec}")
+        #print(f"{target}: RA = {ra}, Dec = {dec}")
+        ra_coord = Angle(ra, unit=u.hour).to_string(unit=u.hour, sep=' ', precision=1)
+        dec_coord = Angle(dec, unit=u.deg).to_string(unit=u.deg, sep=' ', precision=1, alwayssign=True)
+        print(f"{target: <12} {ra_coord: <15} {dec_coord}")
+
+
+
 
     print(f"\nThe best night for observing these targets, avoiding full moon, is: {best_day.to_datetime().strftime('%Y-%m-%d')}")
 
+    print("Saving airmass chart for all targets...")
+    save_airmass_chart(list(zip(targets, ra_dec_list)), best_day.to_datetime(), screenshot_file="airmass_chart.png")
 
 
 if __name__ == "__main__":
