460d1f97ab68d072163f3a41508410757f78daec
Hurum Maksora Tohfa <ttohfa@brynmawr.edu>
2023-05-09 13:15:41 -0700

    Update save_airmass_chart.py

--- save_airmass_chart.py
+++ save_airmass_chart.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By 
 import time
 
 
